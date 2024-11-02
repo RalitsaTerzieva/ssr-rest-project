b400ee90480e7add876efe7ec423fdfe3d0eb37b
Ralica Popova <ralicaterzieva90@gmail.com>
2024-11-02 13:12:31 +0200

    Fix issue and add Sign query logic

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,6 @@
 import { ApolloProvider } from '@apollo/client';
 import { useApollo } from '../../lib/apollo';
+import Head from 'next/head';
 
 export default function App({ Component, pageProps}) {
   const apolloClient = useApollo(pageProps.initialApolloState || {});
@@ -7,7 +8,7 @@ export default function App({ Component, pageProps}) {
   return (
     <ApolloProvider client={apolloClient}>
       <Head>
-        <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet"/>
+      <link href="https://unpkg.com/tailwindcss@latest/dist/tailwind.min.css" rel="stylesheet" />
       </Head>
       <Component {...pageProps} />
     </ApolloProvider>
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,35 +1,52 @@
 import { useEffect } from 'react';
 import Link from 'next/link';
+import { useQuery } from '@apollo/client';
+import { initApollo } from './../../lib/apollo/index';
+import GET_LATEST_SIGNS from './../../lib/apollo/queries/getLatestSigns';
 import axios from 'axios';
+import Sign from '@/components/Sign';
 
 export async function getServerSideProps() {
   const usersReq = await axios.get('https://jsonplaceholder.typicode.com/users');
 
   return {
     props: {
-      users: usersReq.data 
+      users: usersReq.data,
+     
     }
   }
 }
 
 
-function HomePage({ users }) {
- 
+function HomePage() {
+  const { loading, data } = useQuery(GET_LATEST_SIGNS, {
+    fetchPolicy: 'no-cache',
+  });
+  
+  if (loading) {
+    return <>Loading</>
+  }
+  
+  
   return (
     <>
-      <ul>
-      {users.map((user) => (
-          <li key={user.id}>
-              <Link href={`/users/${user.id}`}>
-                  {`${user.username}`}
-              </Link>
-          </li>
-      ))}
-  </ul>
-   <Link href="/joke">
-     Get a Random Joke
- </Link>
- </>
+    <div className="flex justify-center items-center flex-col mt-20">
+      <h1 className="text-3xl mb-5">Real-World Next.js signbook</h1>
+      <Link href="/new-sign">
+        <button className="mb-8 border-2 border-purple-800 text-purple-900 p-2 rounded-lg text-gray-50 m-auto mt-4">
+          Add new sign
+        </button>
+      </Link>
+      <div>
+        {data.sign?.map((sign) => (
+          <Sign key={sign.uuid} {...sign} />
+        ))}
+      </div>
+    </div>
+
+    
+    <Link href="/joke">Get a Random Joke</Link>
+    </>
   )
 }
 
